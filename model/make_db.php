d1b2530fda0dd89bba9991dcd95fbc9c4196947c
James Helms <58826589+jhelms82@users.noreply.github.com>
2021-04-18 21:33:27 -0500

    Update make_db.php

--- model/make_db.php
+++ model/make_db.php
@@ -1,42 +1,49 @@
-<?php 
-    function get_makes() {
-        global $db;
+<?php
+class MakeDB
+{
+    function get_makes()
+    {
+        $db = Database::getDB();
         $query = 'SELECT * FROM makes ORDER BY ID';
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $makes = $statement->fetchAll();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> execute ();
+        $makes = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $makes;
     }
 
-    function get_make_name($make_id) {
-        global $db;
+    function get_make_name($make_id)
+    {
+        $db = Database::getDB();
         $query = 'SELECT * FROM makes WHERE ID = :make_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':make_id', $make_id);
-        $statement->execute();
-        $make = $statement->fetch();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':make_id', $make_id);
+        $statement -> execute ();
+        $make = $statement -> fetch ();
+        $statement -> closeCursor ();
         $make_name = $make['Make'];
         return $make_name;
     }
 
-    function delete_make($make_id) {
-        global $db;
+    function delete_make($make_id)
+    {
+        $db = Database::getDB();
         $query = 'DELETE FROM makes WHERE ID = :make_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':make_id', $make_id);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':make_id', $make_id);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
 
-    function add_make($make_name) {
-        global $db;
+    function add_make($make_name)
+    {
+        $db = Database::getDB();
         $query = 'INSERT INTO makes (Make)
               VALUES
                  (:makeName)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':makeName', $make_name);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':makeName', $make_name);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
+}
