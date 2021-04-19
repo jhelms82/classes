05d99199a02de3311a236849153fa79b2b79094d
James Helms <58826589+jhelms82@users.noreply.github.com>
2021-04-18 21:34:03 -0500

    Update type_db.php

--- model/type_db.php
+++ model/type_db.php
@@ -1,42 +1,49 @@
-<?php 
-    function get_types() {
-        global $db;
+<?php
+class TypeDB
+{
+    function get_types()
+    {
+        $db = Database::getDB();
         $query = 'SELECT * FROM types ORDER BY ID';
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $types = $statement->fetchAll();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> execute ();
+        $types = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $types;
     }
 
-    function get_type_name($type_id) {
-        global $db;
+    function get_type_name($type_id)
+    {
+        $db = Database::getDB();
         $query = 'SELECT * FROM types WHERE ID = :type_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':type_id', $type_id);
-        $statement->execute();
-        $type = $statement->fetch();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':type_id', $type_id);
+        $statement -> execute ();
+        $type = $statement -> fetch ();
+        $statement -> closeCursor ();
         $type_name = $type['Type'];
         return $type_name;
     }
 
-    function delete_type($type_id) {
-        global $db;
+    function delete_type($type_id)
+    {
+        $db = Database::getDB();
         $query = 'DELETE FROM types WHERE ID = :type_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':type_id', $type_id);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':type_id', $type_id);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
 
-    function add_type($type_name) {
-        global $db;
+    function add_type($type_name)
+    {
+        $db = Database::getDB();
         $query = 'INSERT INTO types (Type)
               VALUES
                  (:typeName)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':typeName', $type_name);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':typeName', $type_name);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
+}
