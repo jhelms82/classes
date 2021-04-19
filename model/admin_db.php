faa3ed872d33a03b0c89393108b1864ba2279894
James Helms <58826589+jhelms82@users.noreply.github.com>
2021-04-18 22:09:42 -0500

    Update admin_db.php

--- model/admin_db.php
+++ model/admin_db.php
@@ -1,32 +1,48 @@
 <?php
+class AdminDB {
 
-function add_admin($username, $password) {
-    global $db;
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $query = 'INSERT INTO administrators (username, password)
+
+   public static function add_admin($username, $password)
+    {
+        $db = Database::getDB();
+        $hash = password_hash ($password, PASSWORD_DEFAULT);
+        $query = 'INSERT INTO administrators (username, password)
               VALUES (:username, :password)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $hash);
-    $statement->execute();
-    $statement->closeCursor();
-}
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':username', $username);
+        $statement -> bindValue (':password', $hash);
+        $statement -> execute ();
+        $statement -> closeCursor ();
+    }
 
-function is_valid_admin_login($username, $password) {
-    global $db;
-    $query = 'SELECT password FROM administrators
+
+    public static function is_valid_admin_login($username, $password)
+    {
+        $db = Database::getDB();
+        $query = 'SELECT password FROM administrators
               WHERE username = :username';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->execute();
-    $row = $statement->fetch();
-    $statement->closeCursor();
-    $hash = (!empty($row['password'])) ? $row['password'] : NULL;
-    return password_verify($password, $hash);
-     
-}
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':username', $username);
+        $statement -> execute ();
+        $row = $statement -> fetch ();
+        $statement -> closeCursor ();
+        $hash = (!empty($row['password'])) ? $row['password'] : NULL;
+        return password_verify ($password, $hash);
+
+    }
 
 
-function username_exists($username) {
-            array_push($errors, "The username you entered is already taken.");
+    public static function valid_registration($username)
+    {
+        $db = Database::getDB();
+        $statement = $db -> prepare ("SELECT username FROM administrators WHERE username = :username");
+        $statement -> bindParam (':username', $username);
+        $statement -> execute ();
+
+        if ($statement -> rowCount () > 0) {
+            return true;
+        } else {
+            return false;
         }
+    }
+}
