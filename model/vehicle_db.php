4838ca35f431322988902ef7240d96193351b65a
James Helms <58826589+jhelms82@users.noreply.github.com>
2021-04-18 21:34:40 -0500

    Update vehicle_db.php

--- model/vehicle_db.php
+++ model/vehicle_db.php
@@ -1,12 +1,15 @@
-<?php 
-    function get_vehicles_by_class($class_id, $sort) {
-        global $db;
-        if ($sort == 'year'){
+<?php
+class VehicleDB
+{
+    function get_vehicles_by_class($class_id, $sort)
+    {
+        $db = Database::getDB();
+        if ($sort == 'year') {
             $orderby = 'V.year';
         } else {
             $orderby = 'V.price';
         }
-        
+
         $query = 'SELECT V.ID, V.year, M.Make, V.model, V.price, T.Type, C.Class 
         FROM vehicle V 
         LEFT JOIN makes M ON V.make_id = M.ID 
@@ -14,23 +17,24 @@
         LEFT JOIN types T ON V.type_id = T.ID 
         WHERE V.class_id = :class_id 
         ORDER BY ' . $orderby . ' DESC';
-        
-        $statement = $db->prepare($query);
-        $statement->bindValue(':class_id', $class_id);
-        $statement->execute();
-        $vehicles = $statement->fetchAll();
-        $statement->closeCursor();
+
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':class_id', $class_id);
+        $statement -> execute ();
+        $vehicles = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $vehicles;
     }
 
-    function get_vehicles_by_type($type_id, $sort) {
-        global $db;
-        if ($sort == 'year'){
+    function get_vehicles_by_type($type_id, $sort)
+    {
+        $db = Database::getDB();
+        if ($sort == 'year') {
             $orderby = 'V.year';
         } else {
             $orderby = 'V.price';
         }
-        
+
         $query = 'SELECT V.ID, V.year, M.Make, V.model, V.price, T.Type, C.Class 
         FROM vehicle V 
         LEFT JOIN makes M ON V.make_id = M.ID 
@@ -38,23 +42,24 @@
         LEFT JOIN types T ON V.type_id = T.ID  
         WHERE V.type_id = :type_id 
         ORDER BY ' . $orderby . ' DESC';
-        
-        $statement = $db->prepare($query);
-        $statement->bindValue(':type_id', $type_id);
-        $statement->execute();
-        $vehicles = $statement->fetchAll();
-        $statement->closeCursor();
+
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':type_id', $type_id);
+        $statement -> execute ();
+        $vehicles = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $vehicles;
     }
 
-    function get_vehicles_by_make($make_id, $sort) {
-        global $db;
-        if ($sort == 'year'){
+    function get_vehicles_by_make($make_id, $sort)
+    {
+        $db = Database::getDB();
+        if ($sort == 'year') {
             $orderby = 'V.year';
         } else {
             $orderby = 'V.price';
         }
-        
+
         $query = 'SELECT V.ID, V.year, M.Make, V.model, V.price, T.Type, C.Class 
         FROM vehicle V 
         LEFT JOIN makes M ON V.make_id = M.ID 
@@ -62,18 +67,19 @@
         LEFT JOIN types T ON V.type_id = T.ID  
         WHERE V.make_id = :make_id 
         ORDER BY ' . $orderby . ' DESC';
-        
-        $statement = $db->prepare($query);
-        $statement->bindValue(':make_id', $make_id);
-        $statement->execute();
-        $vehicles = $statement->fetchAll();
-        $statement->closeCursor();
+
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':make_id', $make_id);
+        $statement -> execute ();
+        $vehicles = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $vehicles;
     }
 
-    function get_all_vehicles($sort) {
-        global $db;
-        if ($sort == 'year'){
+    function get_all_vehicles($sort)
+    {
+        $db = Database::getDB();
+        if ($sort == 'year') {
             $orderby = 'V.year';
         } else {
             $orderby = 'V.price';
@@ -84,34 +90,37 @@
             LEFT JOIN classes C ON V.class_id = C.ID 
             LEFT JOIN types T ON V.type_id = T.ID  
             ORDER BY ' . $orderby . ' DESC';
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $vehicles = $statement->fetchAll();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> execute ();
+        $vehicles = $statement -> fetchAll ();
+        $statement -> closeCursor ();
         return $vehicles;
     }
 
-    function delete_vehicle($vehicle_id) {
-        global $db;
+    function delete_vehicle($vehicle_id)
+    {
+        $db = Database::getDB();
         $query = 'DELETE FROM vehicle WHERE ID = :vehicle_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':vehicle_id', $vehicle_id);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':vehicle_id', $vehicle_id);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
 
-    function add_vehicle($make_id, $type_id, $class_id, $year, $model, $price) {
-        global $db;
+    function add_vehicle($make_id, $type_id, $class_id, $year, $model, $price)
+    {
+        $db = Database::getDB();
         $query = 'INSERT INTO vehicle (year, make_id, model, price, type_id, class_id)
               VALUES
                  (:year, :make_id, :model, :price, :type_id, :class_id)';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':year', $year);
-        $statement->bindValue(':make_id', $make_id);
-        $statement->bindValue(':model', $model);
-        $statement->bindValue(':price', $price);
-        $statement->bindValue(':type_id', $type_id);
-        $statement->bindValue(':class_id', $class_id);
-        $statement->execute();
-        $statement->closeCursor();
+        $statement = $db -> prepare ($query);
+        $statement -> bindValue (':year', $year);
+        $statement -> bindValue (':make_id', $make_id);
+        $statement -> bindValue (':model', $model);
+        $statement -> bindValue (':price', $price);
+        $statement -> bindValue (':type_id', $type_id);
+        $statement -> bindValue (':class_id', $class_id);
+        $statement -> execute ();
+        $statement -> closeCursor ();
     }
+}
